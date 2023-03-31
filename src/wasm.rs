bab316c04c70b98a760153a40700274fc9255595
Shane Celis <shane.celis@gmail.com>
2023-03-31 03:00:36 -0400

    hack: It's compiling again.

--- src/wasm.rs
+++ src/wasm.rs
@@ -6,10 +6,15 @@ use embedded_graphics::{
 use embedded_graphics_framebuf::{FrameBuf, backends::FrameBufferBackend};
 extern crate console_error_panic_hook;
 
+use std::cell::RefCell;
+use std::rc::Rc;
+// use std::borrow::Borrow;
+
 const SCREEN_WIDTH: usize = 160;
 const SCREEN_HEIGHT: usize = 128;
 
 use crate::{App, Buttons};
+use web_sys::Window;
 
 /*
  * 1. What is going on here?
@@ -56,8 +61,6 @@ impl FrameBufferBackend for WasmBuffer {
 }
 
 
-use std::cell::Cell;
-use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 
 use embedded_fps::StdClock;
@@ -71,7 +74,7 @@ impl TryDefault<FpsApp<StdClock>> for FpsApp<StdClock> {
 }
 
 pub fn run_with<F,A>(app_maker: F)
-        where F : FnOnce() -> A, A : App {
+        where F : FnOnce() -> A, A : App + 'static {
 
     // if Some("1") == option_env!("SHOW_FPS") {
     //     _run_with(move || app_maker().join(FpsApp::default()));
@@ -81,47 +84,12 @@ pub fn run_with<F,A>(app_maker: F)
 }
 
 fn _run_with<F,A>(app_maker: F)
-        where F : FnOnce() -> A, A : App {
+        where F : FnOnce() -> A, A : App + 'static {
 
     let mut app = app_maker();
 
     app.init().expect("error initializing");
-    let mut buttons = Buttons::empty();
-    setup(app).expect("error setting up app");
-
-    // // 'running: loop {
-    // loop {
-    //     window.update(&display);
-    //     // BUG: This seems to hang on macOS if window.events() is not called.
-    //     for event in window.events() {
-    //         match event {
-    //             SimulatorEvent::KeyDown { keycode, .. } => match keycode {
-    //                 Keycode::W => buttons |= Buttons::W,
-    //                 Keycode::A => buttons |= Buttons::A,
-    //                 Keycode::S => buttons |= Buttons::S,
-    //                 Keycode::D => buttons |= Buttons::D,
-    //                 Keycode::I => buttons |= Buttons::I,
-    //                 Keycode::J => buttons |= Buttons::J,
-    //                 Keycode::K => buttons |= Buttons::K,
-    //                 Keycode::L => buttons |= Buttons::L,
-    //                 _ => {}
-    //             },
-
-    //             SimulatorEvent::KeyUp { keycode, .. } => match keycode {
-    //                 Keycode::W => buttons &= !Buttons::W,
-    //                 Keycode::A => buttons &= !Buttons::A,
-    //                 Keycode::S => buttons &= !Buttons::S,
-    //                 Keycode::D => buttons &= !Buttons::D,
-    //                 Keycode::I => buttons &= !Buttons::I,
-    //                 Keycode::J => buttons &= !Buttons::J,
-    //                 Keycode::K => buttons &= !Buttons::K,
-    //                 Keycode::L => buttons &= !Buttons::L,
-    //                 _ => {}
-    //             },
-    //             SimulatorEvent::Quit => panic!("quit"), //break 'running,
-    //             _ => {}
-    //         }
-    //     }
+    setup::<A>(app).expect("error setting up app");
 
     //     app.update(buttons).expect("error updating");
     //     app.draw(&mut display).expect("error drawing");
@@ -129,9 +97,15 @@ fn _run_with<F,A>(app_maker: F)
     // }
 }
 
+fn animation_frame(w: &Window, f: &Closure<dyn FnMut()>) {
+    w.request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register 'requestAnimationFrame` OK");
+}
+
+// #[wasm_bindgen]
 // Main is called when the wasm module is instantiated.
 // #[wasm_bindgen(start)]
-fn setup<A>(app : A) -> Result<(), JsValue> where A : App{
+fn setup<A>(mut app : A) -> Result<(), JsValue> where A : App + 'static {
     console_error_panic_hook::set_once();
 
     let mut data = WasmBuffer([0; OUTPUT_BUFFER_SIZE]);
@@ -166,69 +140,120 @@ fn setup<A>(app : A) -> Result<(), JsValue> where A : App{
         .get_context("2d")?
         .unwrap()
         .dyn_into::<web_sys::CanvasRenderingContext2d>()?;
+    let buttons = Rc::new(RefCell::new(Buttons::empty()));
     let context = Rc::new(context);
     let body = Rc::new(body);
-    let document = Rc::new(document);
-    let pressed = Rc::new(Cell::new(false));
     {
-        let context = context.clone();
-        let pressed = pressed.clone();
-        let closure = Closure::<dyn FnMut(_)>::new(move |event: web_sys::MouseEvent| {
-            context.begin_path();
-            context.move_to(event.offset_x() as f64, event.offset_y() as f64);
-            pressed.set(true);
-        });
-        canvas.add_event_listener_with_callback("mousedown", closure.as_ref().unchecked_ref())?;
-        closure.forget();
-    }
-    {
-        let context = context.clone();
-        let pressed = pressed.clone();
-        let closure = Closure::<dyn FnMut(_)>::new(move |event: web_sys::MouseEvent| {
-            if pressed.get() {
-                context.line_to(event.offset_x() as f64, event.offset_y() as f64);
-                context.stroke();
-                context.begin_path();
-                context.move_to(event.offset_x() as f64, event.offset_y() as f64);
-            }
-        });
-        canvas.add_event_listener_with_callback("mousemove", closure.as_ref().unchecked_ref())?;
-        closure.forget();
-    }
-    {
-        let context = context.clone();
-        let pressed = pressed.clone();
-        let closure = Closure::<dyn FnMut(_)>::new(move |event: web_sys::MouseEvent| {
-            pressed.set(false);
-            context.line_to(event.offset_x() as f64, event.offset_y() as f64);
-            context.stroke();
-        });
-        canvas.add_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref())?;
-        closure.forget();
-    }
-
-    {
-        let body = body.clone();
-        let documentC = document.clone();
+        let buttons = buttons.clone();
         let closure = Closure::<dyn FnMut(_)>::new(move |e: web_sys::KeyboardEvent| {
-            let c = char::from(e.key_code() as u8).to_ascii_lowercase();
+            let c = char::from(e.key_code() as u8).to_ascii_uppercase();
+            buttons.replace_with(move |buttons|
             match c {
-                'w' => {
-            let p = documentC.create_element("p").unwrap();
-            p.set_inner_html("w");
-            body.append_child(&p).unwrap();
-                }
-                _ => {
-            let p = documentC.create_element("p").unwrap();
-            p.set_inner_html("wtf!");
-            body.append_child(&p).unwrap();
-                }
-
-            };
+                'W' => *buttons | Buttons::W,
+                'A' => *buttons | Buttons::A,
+                'S' => *buttons | Buttons::S,
+                'D' => *buttons | Buttons::D,
+                'I' => *buttons | Buttons::I,
+                'J' => *buttons | Buttons::J,
+                'K' => *buttons | Buttons::K,
+                'L' => *buttons | Buttons::L,
+                _ => *buttons
             });
+        });
         document.add_event_listener_with_callback("keypress", closure.as_ref().unchecked_ref())?;
         closure.forget();
     }
 
+    {
+        let buttons = buttons.clone();
+        let closure = Closure::<dyn FnMut(_)>::new(move |e: web_sys::KeyboardEvent| {
+            let c = char::from(e.key_code() as u8).to_ascii_uppercase();
+            buttons.replace_with(move |buttons|
+            match c {
+                'W' => *buttons & !Buttons::W,
+                'A' => *buttons & !Buttons::A,
+                'S' => *buttons & !Buttons::S,
+                'D' => *buttons & !Buttons::D,
+                'I' => *buttons & !Buttons::I,
+                'J' => *buttons & !Buttons::J,
+                'K' => *buttons & !Buttons::K,
+                'L' => *buttons & !Buttons::L,
+                _ => *buttons
+            });
+        });
+        document.add_event_listener_with_callback("keyup", closure.as_ref().unchecked_ref())?;
+        closure.forget();
+    }
+    // {
+    // // Here we want to call `requestAnimationFrame` in a loop, but only a fixed
+    // // number of times. After it's done we want all our resources cleaned up. To
+    // // achieve this we're using an `Rc`. The `Rc` will eventually store the
+    // // closure we want to execute on each frame, but to start out it contains
+    // // `None`.
+    // //
+    // // After the `Rc` is made we'll actually create the closure, and the closure
+    // // will reference one of the `Rc` instances. The other `Rc` reference is
+    // // used to store the closure, request the first frame, and then is dropped
+    // // by this function.
+    // //
+    // // Inside the closure we've got a persistent `Rc` reference, which we use
+    // // for all future iterations of the loop
+    // // let f : Rc<RefCell<Option<Closure<dyn FnMut()>>>> = Rc::new(RefCell::new(None));
+    // let f = Rc::new(RefCell::new(None));
+    // let g = f.clone();
+    // let buttons = buttons.clone();
+    // let window = Rc::new(window);
+    // let win = window.clone();
+
+    // // let mut i = 0;
+    // *g.borrow_mut() = Some(Closure::new(Box::new(move || {
+    //     // if i > 300 {
+    //     //     body().set_text_content(Some("All done!"));
+
+    //     //     // Drop our handle to this closure so that it will get cleaned
+    //     //     // up once we return.
+    //     //     let _ = f.borrow_mut().take();
+    //     //     return;
+    //     // }
+    //     // app.update((*buttons).borrow().clone());
+
+    //     // Set the body's text content to how many times this
+    //     // requestAnimationFrame callback has fired.
+    //     // i += 1;
+
+
+    //     // Schedule ourself for another requestAnimationFrame callback.
+    //     animation_frame(&win, f.borrow().as_ref().unwrap());
+    // }) as Box<dyn FnMut()>));
+
+    // animation_frame(&window, g.borrow().as_ref().unwrap());
+    // }
+
+
+    let init : Option<Closure<dyn FnMut()>> = None;
+    let f = Rc::new(RefCell::new(init));
+    let g = f.clone();
+
+    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+
+        // Schedule ourself for another requestAnimationFrame callback.
+        // let b : RefCell<Option<Closure<dyn FnMut()>>>= *f;
+        request_animation_frame(f.borrow().as_ref().unwrap());
+
+    }) as Box<dyn FnMut()>));// <dyn FnMut()>);
+
+    request_animation_frame(g.borrow().as_ref().unwrap());
+
+
     Ok(())
+}
+
+fn window() -> web_sys::Window {
+    web_sys::window().expect("no global `window` exists")
+}
+
+fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("should register `requestAnimationFrame` OK");
 }
