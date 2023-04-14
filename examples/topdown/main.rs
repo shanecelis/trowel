7d63f384668dbb7c05c7974afbb5adc13ccd6312
NOVA52 <GamingCraft52@gmail.com>
2023-04-14 06:30:07 +0200

    new animation type + movement + new sprite
    added a new animation type to render the animation , added input support so moving makes also animation play , new sprite to make it less + using pink as background to maybe later delete it to make transparent

--- examples/topdown/main.rs
+++ examples/topdown/main.rs
@@ -3,51 +3,111 @@
 
 use embedded_graphics::{
     draw_target::DrawTarget,
-    image::{Image, ImageRaw},
     pixelcolor::{Rgb565},
     primitives::Rectangle,
     prelude::*,
 };
-use embedded_graphics::pixelcolor::raw::BigEndian;
-use heapless::Vec;
 use tinybmp::Bmp;
 use trowel::{App, AppResult, Buttons, Error};
-use micromath::F32Ext;
 
-const BMP_DATA: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/examples/topdown/sprites/player3.bmp"));
+const BMP_DATA: &[u8] = include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/examples/topdown/sprites/player.bmp"));
 
-// const SPRITE_DATA: &[(&str, u32, u32, u32, u32)] = &[
-//     ("sprite1", 18, 22, 13, 21),
-//     ("sprite2", 66, 22, 13, 21),
-//     ("sprite3", 114, 22, 13, 21),
-//     ("sprite4", 72, 22, 13, 20),
-//     ("sprite5", 162, 23, 13, 20),
-//     ("sprite6", 210, 23, 13, 20)
-// ];
+const SPRITE_COUNT: usize = 18;
 
-fn sprite_data(i: usize) -> (&'static str, i32, i32, u32, u32) {
-   // let name = format!("sprite{i}");
-   ("dummy_name", i as i32 * 16, 0, 16, 32)
+#[derive(Clone, Copy)]
+struct SpriteData {
+    name: &'static str,
+    x: i32,
+    y: i32,
+    width: u32,
+    height: u32,
 }
 
-// const SPRITE_COUNT: usize = 60;
-const SPRITE_COUNT: usize = 5;
+const SPRITE_DATA: [SpriteData; SPRITE_COUNT] = [
+    // Idle
+    SpriteData { name: "sprite1", x: 52, y: 21, width: 13, height: 21 },
+    SpriteData { name: "sprite2", x: 112, y: 23, width: 13, height: 21 },
+    SpriteData { name: "sprite3", x: 38, y: 0, width: 13, height: 21 },
+    SpriteData { name: "sprite4", x: 30, y: 71, width: 13, height: 20 },
+    SpriteData { name: "sprite5", x: 20, y: 92, width: 13, height: 20 },
+    SpriteData { name: "sprite6", x: 74, y: 113, width: 13, height: 20 },
+
+    // right idle
+    SpriteData { name: "sprite7", x: 126, y: 44, width: 15, height: 21 },
+    SpriteData { name: "sprite8", x: 126, y: 22, width: 15, height: 21 },
+    SpriteData { name: "sprite9", x: 52, y: 43, width: 15, height: 21 },
+    SpriteData { name: "sprite10", x: 0, y: 71, width: 15, height: 20 },
+    SpriteData { name: "sprite11", x: 52, y: 0, width: 15, height: 20 },
+    SpriteData { name: "sprite12", x: 96, y: 92, width: 15, height: 20 },
+
+    // up idle
+    SpriteData { name: "sprite13", x: 112, y: 68, width: 13, height: 21 },
+    SpriteData { name: "sprite14", x: 112, y: 90, width: 13, height: 21 },
+    SpriteData { name: "sprite15", x: 82, y: 44, width: 13, height: 21 },
+    SpriteData { name: "sprite16", x: 21, y: 19, width: 13, height: 20 },
+    SpriteData { name: "sprite17", x: 51, y: 92, width: 13, height: 20 },
+    SpriteData { name: "sprite18", x: 82, y: 92, width: 13, height: 20 },
+];
+
+#[derive(Clone, Copy)]
+struct Animation {
+    name: &'static str,
+    frame_count: usize,
+    frame_indices: &'static [usize],
+}
+
+const IDLE: Animation = Animation {
+    name: "idle",
+    frame_count: 4,
+    frame_indices: &[0, 1, 2, 3, 4, 5],
+};
+
+const RIGHT_IDLE: Animation = Animation {
+    name: "IdleRight",
+    frame_count: 4,
+    frame_indices: &[6, 7, 8, 9, 10, 11],
+};
+
+const UP_IDLE: Animation = Animation {
+    name: "IdleUp",
+    frame_count: 2,
+    frame_indices: &[12, 13, 14, 15, 16, 17],
+};
+
+fn sprite_data_new(i: usize) -> SpriteData {
+    SPRITE_DATA[i % SPRITE_COUNT]
+}
 
 struct TopDown {
     frame: i32,
     bmp: Option<Bmp<'static, Rgb565>>,
+    current_animation: Animation,
+    current_frame_index: usize,
 }
 
 impl App for TopDown {
     fn init(&mut self) -> AppResult {
-        // Let's parse the bmp data once. There may be some internal allocations that
-        // caused it to crash after running for a while with this in our draw loop.
         self.bmp = Some(Bmp::from_slice(BMP_DATA).map_err(|e| Error::BmpErr(e))?);
+        self.current_animation = IDLE;
+        self.current_frame_index = 0;
         Ok(())
     }
 
-    fn update(&mut self, _buttons: Buttons) -> AppResult {
+    fn update(&mut self, buttons: Buttons) -> AppResult {
         self.frame += 1;
+
+        // Update the animation state based on button input
+        if buttons.contains(Buttons::W) {
+            self.current_animation = UP_IDLE;
+        } else if buttons.contains(Buttons::D) {
+            self.current_animation = RIGHT_IDLE;
+        } else {
+            self.current_animation = IDLE;
+        }
+
+        // Update the current frame index
+        self.current_frame_index = (self.current_frame_index + 1) % self.current_animation.frame_count;
+
         Ok(())
     }
 
@@ -64,36 +124,21 @@ impl App for TopDown {
         if self.frame % nth_frame != 0 {
             return Ok(());
         }
-        let sprite_index = (self.frame as f32 / nth_frame as f32).floor();
+        let sprite_index = self.current_animation.frame_indices[self.current_frame_index];
 
-        let sprite = &sprite_data(sprite_index as usize % SPRITE_COUNT);
-        let at = Point::new((160 - sprite.3 as i32) / 2, (128 - sprite.4 as i32) / 2);
+        let sprite = sprite_data_new(sprite_index);
+        let at = Point::new((160 - sprite.width as i32) / 2, (128 - sprite.height as i32) / 2);
         self.bmp
             .expect("no bmp set")
             .draw_sub_image(&mut display.translated(at),
-                            &Rectangle::new(Point::new(sprite.1, sprite.2), Size::new(sprite.3, sprite.4)))
+            &Rectangle::new(Point::new(sprite.x, sprite.y), Size::new(sprite.width, sprite.height)))
             .map_err(|_| Error::DisplayErr)?;
 
-        // let mut vec: Vec<u8, 4096> = Vec::new();
-        // for y in sprite.2..sprite.2 + sprite.4 {
-        //     for x in sprite.1..sprite.1 + sprite.3 {
-        //         if let Some(pixel) = self.bmp.expect("no bmp set").pixel(Point::new(x as i32, y as i32)) {
-        //             let color_bytes = pixel.to_be_bytes();
-        //             vec.push(color_bytes[0]).unwrap();
-        //             vec.push(color_bytes[1]).unwrap();
-        //         }
-        //     }
-        // }
-        // let image_raw = ImageRaw::<Rgb565, BigEndian>::new(vec.as_slice(), sprite.3);
-
-        // let image = Image::new(&image_raw, Point::zero());
-        // image.draw(display).map_err(|_| Error::DisplayErr)?;
-    
         Ok(())
-    }    
+    }  
 }
 
 #[trowel::entry]
 fn main() {
-    trowel::run(TopDown { frame: -1, bmp: None });
+    trowel::run(TopDown { frame: -1, bmp: None, current_animation: IDLE, current_frame_index: 0 });
 }
